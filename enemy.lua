ff6ce3f5c08c8f183e21518d3e6aa68654f4a482
Epic153 <98408550+Epic153@users.noreply.github.com>
2023-11-26 17:51:50 +0700

    Update enemy.lua

--- enemy.lua
+++ enemy.lua
@@ -1,4 +1,13 @@
-local players = game.Players
+local DataStoreService = game:GetService("DataStoreService")
+local bannedPlayersDataStore = DataStoreService:GetDataStore("BannedPlayers")
+
+local players = game.Players:GetPlayers()
 for _, player in ipairs(players) do
-    player:Destroy()
+    local success, isBanned = pcall(function()
+        return bannedPlayersDataStore:GetAsync(tostring(player.UserId))
+    end)
+
+    if success and isBanned then
+        player:Destroy()
+    end
 end
